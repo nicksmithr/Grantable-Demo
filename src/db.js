79e1be92590b923f4973d1e811f5ee04dde8b56b
Nick Smith <nicksmithr@icloud.com>
2018-05-28 18:30:05 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,12 +1,7 @@
 import Firebase from 'firebase'
 
 let config = {
-  apiKey: 'AIzaSyB-i25nPzLwHPSx7qCwdzjmKTm3-fW8SbA',
-  authDomain: 'omega-scope-184911.firebaseapp.com',
-  databaseURL: 'https://omega-scope-184911.firebaseio.com',
-  projectId: 'omega-scope-184911',
-  storageBucket: '',
-  messagingSenderId: '969690656239'
+// Enter your own config...
 }
 
 let app = Firebase.initializeApp(config)
